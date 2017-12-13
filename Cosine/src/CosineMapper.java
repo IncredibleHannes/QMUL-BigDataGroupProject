9b6f4325375a717cd36e4208335870aed96a5839
liamsscreenname <liamglkelly@gmail.com>
2017-12-13 14:02:48 +0000

    Fixed spelling mistake

--- Cosine/src/CosineMapper.java
+++ Cosine/src/CosineMapper.java
@@ -44,7 +44,7 @@ public class CosineMapper extends Mapper<Object, Text, Text, Text> {
     int moviesSize = movies.size();
     for (int i = 0; i < moviesSize; i++) {
       for (int j = i; j < moviesSize; j++) {
-        if (movie.get(i) > movies.get(j)) {
+        if (movies.get(i) > movies.get(j)) {
           context.write(new Text(movies.get(i)+","+ movies.get(j)), new Text(ratings.get(i)+","+ratings.get(j)+","+ average));
         } else {
           context.write(new Text(movies.get(j)+","+ movies.get(i)), new Text(ratings.get(j)+","+ratings.get(i)+","+ average));
